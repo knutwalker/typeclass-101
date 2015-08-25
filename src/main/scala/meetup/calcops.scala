764ebb40697881ba30edfd5a9f42db2b8d386c8e
Paul Horn <knutwalker@gmail.com>
2015-08-25 23:35:52 +0200

    Problem 1.1: Add a new type

--- src/main/scala/meetup/calcops.scala
+++ src/main/scala/meetup/calcops.scala
@@ -15,6 +15,10 @@ package calcops {
       def add(y: Int32): Int32 = Int32(x + y.x)
       def mult(y: Int32): Int32 = Int32(x * y.x)
     }
+    case class Int64(x: Long) extends Operand[Int64] {
+      def add(y: Int64): Int64 = Int64(x + y.x)
+      def mult(y: Int64): Int64 = Int64(x * y.x)
+    }
     case class Real32(x: Float) extends Operand[Real32] {
       def add(y: Real32): Real32 = Real32(x + y.x)
       def mult(y: Real32): Real32 = Real32(x * y.x)
@@ -25,6 +29,7 @@ package calcops {
 
     sealed trait Operand
     case class Int32(x: Int) extends Operand
+    case class Int64(x: Long) extends Operand
     case class Real32(x: Float) extends Operand
 
     // ================
@@ -32,12 +37,14 @@ package calcops {
     def add[A <: Operand](left: A)(right: A): A =
       (left, right) match {
         case (Int32(x), Int32(y))   => Int32(x + y).asInstanceOf[A]
+        case (Int64(x), Int64(y))   => Int64(x + y).asInstanceOf[A]
         case (Real32(x), Real32(y)) => Real32(x + y).asInstanceOf[A]
       }
 
     def mult[A <: Operand](left: A)(right: A): A =
       (left, right) match {
         case (Int32(x), Int32(y))   => Int32(x * y).asInstanceOf[A]
+        case (Int64(x), Int64(y))   => Int64(x * y).asInstanceOf[A]
         case (Real32(x), Real32(y)) => Real32(x * y).asInstanceOf[A]
       }
   }
