68ffa90d3d46ef8a015550b609740531cd53a4b2
Paul Horn <knutwalker@gmail.com>
2015-08-25 23:56:29 +0200

    On the way to the Monoid: WET the code a bit

--- src/main/scala/meetup/monoid.scala
+++ src/main/scala/meetup/monoid.scala
@@ -9,4 +9,10 @@ package object monoid {
     case x :: rest => x + sum(rest)
     case Nil       => 0
   }
+
+  // fails: WET code, generally also type erasure issues
+  def sum(xs: List[Double]): Double = xs match {
+    case x :: rest => x + sum(rest)
+    case Nil       => 0.0d
+  }
 }
