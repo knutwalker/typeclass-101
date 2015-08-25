7cbafdeefaae513f75d2fe0e973d33cf6d0d25cd
Paul Horn <knutwalker@gmail.com>
2015-08-26 00:45:30 +0200

    On the way to the Foldable: Right to the type class

--- src/main/scala/meetup/foldable.scala
+++ src/main/scala/meetup/foldable.scala
@@ -1,9 +1,32 @@
 package meetup
 
+import language.higherKinds
+
 package object foldable {
   import monoid.Monoid
 
+  trait Fold[F[_]] {
+    def foldl[A, B](fa: F[A])(zero: B)(f: (B, A) => B): B
+  }
+  object Fold {
+    def apply[F[_]](implicit F: Fold[F]): Fold[F] = F
 
-  def sum[A: Monoid](xs: Traversable[A]): A =
-    xs.foldLeft(Monoid[A].zero)(Monoid[A].add)
+    implicit object ListFold extends Fold[List] {
+      def foldl[A, B](fa: List[A])(zero: B)(f: (B, A) => B): B =
+        fa.foldLeft(zero)(f)
+    }
+
+    implicit object VectorFold extends Fold[Vector] {
+      def foldl[A, B](fa: Vector[A])(zero: B)(f: (B, A) => B): B =
+        fa.foldLeft(zero)(f)
+    }
+  }
+  implicit class FoldOps[F[_], A](val self: F[A]) extends AnyVal {
+    def foldl[B](zero: B)(f: (B, A) => B)(implicit F: Fold[F]): B =
+      F.foldl(self)(zero)(f)
+  }
+
+
+  def sum[F[_]: Fold, A: Monoid](xs: F[A]): A =
+    xs.foldl(Monoid[A].zero)(Monoid[A].add)
 }
