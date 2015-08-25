0d34273f26d75e483010cb90c745399c520fc936
Paul Horn <knutwalker@gmail.com>
2015-08-25 23:40:15 +0200

    Problem 1.3: Type classes (to the rescue)

--- src/main/scala/meetup/calcops.scala
+++ src/main/scala/meetup/calcops.scala
@@ -59,4 +59,63 @@ package calcops {
         case (Real32(x), Real32(y)) => Real32(x * y).asInstanceOf[A]
       }
   }
+
+  object typeclass {
+
+    trait CanAdd[A] {
+      def add(x: A, y: A): A
+    }
+
+    // -------------
+
+    trait CanMult[A] {
+      def mult(x: A, y: A): A
+    }
+
+    // ==================
+    // syntax
+
+    implicit final class CanAddOps[A](val x: A) extends AnyVal {
+      def add(y: A)(implicit A: CanAdd[A]): A =
+        A.add(x, y)
+    }
+
+    // -------------
+
+    implicit final class CanMultOps[A](val x: A) extends AnyVal {
+      def mult(y: A)(implicit A: CanMult[A]): A =
+        A.mult(x, y)
+    }
+
+    // =================
+
+    sealed trait Operand
+    case class Int32(x: Int) extends Operand
+    case class Real32(x: Float) extends Operand
+
+    // -----------------
+    // instances
+
+    implicit object Int32CanCalculate extends CanAdd[Int32] with CanMult[Int32] {
+      def add(x: Int32, y: Int32) = Int32(x.x + y.x)
+      def mult(x: Int32, y: Int32) = Int32(x.x * y.x)
+    }
+
+    // -------------
+
+    implicit object Real32CanCalculate extends CanAdd[Real32] with CanMult[Real32] {
+      def add(x: Real32, y: Real32) = Real32(x.x + y.x)
+      def mult(x: Real32, y: Real32) = Real32(x.x * y.x)
+    }
+
+    // ==================
+
+    def add[A: CanAdd](left: A, right: A) =
+      left add right
+
+    // -------------
+
+    def mult[A: CanMult](left: A, right: A) =
+      left mult right
+  }
 }
--- src/test/scala/meetup/CalculatorOpsSpec.scala
+++ src/test/scala/meetup/CalculatorOpsSpec.scala
@@ -44,4 +44,24 @@ class CalculatorOpsSpec extends FunSpec with Matchers {
       "mult(Real32(42.0f))(Int32(1337))" shouldNot typeCheck
     }
   }
+
+  describe("typeclass style operands") {
+    import typeclass._
+
+    it("should add to ints") {
+      (Int32(42) add Int32(1337)) should equal (Int32(42 + 1337))
+    }
+
+    it("should not allow to add ints and reals") {
+      "(Int32(42) add Real32(1337.0f))" shouldNot typeCheck
+    }
+
+    it("should multiply to reals") {
+      mult(Real32(42.0f), Real32(1337.0f)) should equal (Real32(42.0f * 1337.0f))
+    }
+
+    it("should not allow to multiply reals and ints") {
+      "mult(Real32(42.0f), Int32(1337))" shouldNot typeCheck
+    }
+  }
 }
