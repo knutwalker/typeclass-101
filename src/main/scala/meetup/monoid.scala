9bfeace9c100147853627f25e12ac96d3e3ca5b4
Paul Horn <knutwalker@gmail.com>
2015-08-26 00:12:01 +0200

    On the way to the Monoid: The grat summoning

--- src/main/scala/meetup/monoid.scala
+++ src/main/scala/meetup/monoid.scala
@@ -9,23 +9,25 @@ package object monoid {
     def zero: A
     def add(x: A, y: A): A
   }
+  object NumberLike {
+    def apply[A](implicit A: NumberLike[A]): NumberLike[A] = A
 
-  implicit object IntNumber extends NumberLike[Int] {
-    def zero: Int = 0
-    def add(x: Int, y: Int): Int = x + y
-  }
-  implicit object DoubleNumber extends NumberLike[Double] {
-    def zero: Double = 0.0
-    def add(x: Double, y: Double): Double = x + y
+    implicit object IntNumber extends NumberLike[Int] {
+      def zero: Int = 0
+      def add(x: Int, y: Int): Int = x + y
+    }
+    implicit object DoubleNumber extends NumberLike[Double] {
+      def zero: Double = 0.0
+      def add(x: Double, y: Double): Double = x + y
+    }
   }
 
   implicit class NumberLikeOps[A](val x: A) extends AnyVal {
     def +(y: A)(implicit A: NumberLike[A]) = A.add(x, y)
   }
-  def zero[A: NumberLike] = implicitly[NumberLike[A]].zero
 
   def sum[A: NumberLike](xs: List[A]): A = xs match {
     case x :: rest => x + sum(rest)
-    case Nil       => zero[A]
+    case Nil       => NumberLike[A].zero
   }
 }
