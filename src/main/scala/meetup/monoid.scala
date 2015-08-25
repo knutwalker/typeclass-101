22478c23d28cbfeee2da59d7048a587122989d72
Paul Horn <knutwalker@gmail.com>
2015-08-26 00:04:28 +0200

    On the way to the Monoid: Adapter pattern, no no no

--- src/main/scala/meetup/monoid.scala
+++ src/main/scala/meetup/monoid.scala
@@ -5,9 +5,25 @@ package object monoid {
   // add a bunch of things together
 
 
-  // fails: no supertype, looses typeinfo, how to get 0
-  // def sum(xs: List[Number]): Number = xs match {
-  //   case x :: rest => x + sum(rest)
-  //   case Nil       => ???
-  // }
+  // fails: needs complex wrapper everytime, how to get 0
+  trait NumberLike[A] {
+    def +(to: A): A
+  }
+
+  class IntNumber(val underlying: Int) extends NumberLike[IntNumber] {
+    def +(to: IntNumber): IntNumber = new IntNumber(underlying + to.underlying)
+  }
+  class DoubleNumber(val underlying: Double) extends NumberLike[DoubleNumber] {
+    def +(to: DoubleNumber): DoubleNumber = new DoubleNumber(underlying + to.underlying)
+  }
+
+  def sumNumber[A <: NumberLike[A]](xs: List[A])(zero: A): A = xs match {
+    case x :: rest => x + sumNumber(rest)(zero)
+    case Nil       => zero
+  }
+
+  def sum(xs: List[Int]): Int =
+    sumNumber(xs map (new IntNumber(_)))(new IntNumber(0)).underlying
+  def sum(xs: List[Double]): Double =
+    sumNumber(xs map (new DoubleNumber(_)))(new DoubleNumber(0)).underlying
 }
