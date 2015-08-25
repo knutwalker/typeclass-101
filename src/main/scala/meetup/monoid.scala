d54fb946a782225aee868df371d809e5baaa8d57
Paul Horn <knutwalker@gmail.com>
2015-08-26 00:13:14 +0200

    On the way to the Monoid: Hooray

--- src/main/scala/meetup/monoid.scala
+++ src/main/scala/meetup/monoid.scala
@@ -5,29 +5,29 @@ package object monoid {
   // add a bunch of things together
 
 
-  trait NumberLike[A] {
+  trait Monoid[A] {
     def zero: A
     def add(x: A, y: A): A
   }
-  object NumberLike {
-    def apply[A](implicit A: NumberLike[A]): NumberLike[A] = A
+  object Monoid {
+    def apply[A](implicit A: Monoid[A]): Monoid[A] = A
 
-    implicit object IntNumber extends NumberLike[Int] {
+    implicit object IntNumber extends Monoid[Int] {
       def zero: Int = 0
       def add(x: Int, y: Int): Int = x + y
     }
-    implicit object DoubleNumber extends NumberLike[Double] {
+    implicit object DoubleNumber extends Monoid[Double] {
       def zero: Double = 0.0
       def add(x: Double, y: Double): Double = x + y
     }
   }
 
-  implicit class NumberLikeOps[A](val x: A) extends AnyVal {
-    def +(y: A)(implicit A: NumberLike[A]) = A.add(x, y)
+  implicit class MonoidOps[A](val x: A) extends AnyVal {
+    def +(y: A)(implicit A: Monoid[A]) = A.add(x, y)
   }
 
-  def sum[A: NumberLike](xs: List[A]): A = xs match {
+  def sum[A: Monoid](xs: List[A]): A = xs match {
     case x :: rest => x + sum(rest)
-    case Nil       => NumberLike[A].zero
+    case Nil       => Monoid[A].zero
   }
 }
