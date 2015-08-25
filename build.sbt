1172802d2f1e8229aa340bf2295dc5a8e6538401
Paul Horn <knutwalker@gmail.com>
2015-08-26 00:37:29 +0200

    On the way to the Monoid: Sorry, could not resist

--- build.sbt
+++ build.sbt
@@ -2,4 +2,5 @@ name := """typeclasses"""
 version := "1.0"
 scalaVersion := "2.11.7"
 
+libraryDependencies += "com.chuusai" %% "shapeless" % "2.2.5"
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.5" % "test"
--- src/main/scala/meetup/monoid.scala
+++ src/main/scala/meetup/monoid.scala
@@ -1,5 +1,7 @@
 package meetup
 
+import shapeless._
+
 package object monoid {
 
   // add a bunch of things together
@@ -24,12 +26,36 @@ package object monoid {
       def zero: Double = 0.0
       def add(x: Double, y: Double): Double = x + y
     }
+
+    implicit def derive[A, Repr](implicit
+      gen: Generic.Aux[A, Repr],
+      repr: Lazy[Monoid[Repr]])
+    : Monoid[A] = new Monoid[A] {
+      def zero = gen.from(repr.value.zero)
+      def add(x: A, y: A) = gen.from(repr.value.add(gen.to(x), gen.to(y)))
+    }
+
+    implicit val proveHNil
+    : Monoid[HNil] = new Monoid[HNil] {
+      def zero = HNil
+      def add(x: HNil, y: HNil) = HNil
+    }
+
+    implicit def proveHCons[H, T <: HList](implicit
+      H: Lazy[Monoid[H]],
+      T: Lazy[Monoid[T]])
+    : Monoid[H :: T] = new Monoid[H :: T] {
+      def zero = H.value.zero :: T.value.zero
+      def add(x: H :: T, y: H :: T): H :: T =
+        H.value.add(x.head, y.head) :: T.value.add(x.tail, y.tail)
+    }
   }
 
   implicit class SemigroupOps[A](val x: A) extends AnyVal {
     def |+|(y: A)(implicit A: Semigroup[A]) = A.add(x, y)
   }
 
+  import HList.ListCompat._
   def sum[A: Monoid](xs: List[A]): A = xs match {
     case x :: rest => x |+| sum(rest)
     case Nil       => Monoid[A].zero
