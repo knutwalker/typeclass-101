b89fc62c976e1970e80e9380a41e164507c1dac5
Paul Horn <knutwalker@gmail.com>
2015-08-26 00:23:06 +0200

    On the way to the Foldable: I can haz Vector now!

--- src/main/scala/meetup/foldable.scala
+++ src/main/scala/meetup/foldable.scala
@@ -4,6 +4,6 @@ package object foldable {
   import monoid.Monoid
 
 
-  def sum[A: Monoid](xs: List[A]): A =
+  def sum[A: Monoid](xs: Traversable[A]): A =
     xs.foldLeft(Monoid[A].zero)(Monoid[A].add)
 }
