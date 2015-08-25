78fd7d47570d7a30fc4f2a9266991f0c6821e1fe
Paul Horn <knutwalker@gmail.com>
2015-08-25 23:37:11 +0200

    Problem 1.2: Add new operation

--- src/main/scala/meetup/calcops.scala
+++ src/main/scala/meetup/calcops.scala
@@ -6,6 +6,7 @@ package calcops {
 
     trait Operand[A <: Operand[A]] {
       def add(y: A): A
+      def sub(y: A): A
       def mult(y: A): A
     }
 
@@ -13,14 +14,17 @@ package calcops {
 
     case class Int32(x: Int) extends Operand[Int32] {
       def add(y: Int32): Int32 = Int32(x + y.x)
+      def sub(y: Int32): Int32 = Int32(x - y.x)
       def mult(y: Int32): Int32 = Int32(x * y.x)
     }
     case class Int64(x: Long) extends Operand[Int64] {
       def add(y: Int64): Int64 = Int64(x + y.x)
+      def sub(y: Int64): Int64 = Int64(x - y.x)
       def mult(y: Int64): Int64 = Int64(x * y.x)
     }
     case class Real32(x: Float) extends Operand[Real32] {
       def add(y: Real32): Real32 = Real32(x + y.x)
+      def sub(y: Real32): Real32 = Real32(x - y.x)
       def mult(y: Real32): Real32 = Real32(x * y.x)
     }
   }
@@ -39,6 +43,13 @@ package calcops {
         case (Int32(x), Int32(y))   => Int32(x + y).asInstanceOf[A]
         case (Int64(x), Int64(y))   => Int64(x + y).asInstanceOf[A]
         case (Real32(x), Real32(y)) => Real32(x + y).asInstanceOf[A]
+      }
+
+    def sub[A <: Operand](left: A)(right: A): A =
+      (left, right) match {
+        case (Int32(x), Int32(y))   => Int32(x - y).asInstanceOf[A]
+        case (Int64(x), Int64(y))   => Int64(x - y).asInstanceOf[A]
+        case (Real32(x), Real32(y)) => Real32(x - y).asInstanceOf[A]
       }
 
     def mult[A <: Operand](left: A)(right: A): A =
