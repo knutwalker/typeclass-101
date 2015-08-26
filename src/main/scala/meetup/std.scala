55736e2ab6a4966510730ddf2d8cc3fb23bd3fcc
Paul Horn <knutwalker@gmail.com>
2015-08-26 20:49:32 +0200

    Remake the std stuff: add comparable

--- src/main/scala/meetup/std.scala
+++ src/main/scala/meetup/std.scala
@@ -14,11 +14,26 @@ package object std {
     }
   }
 
+  trait Comparable[A] {
+    def lte(x: A, y: A): Boolean
+  }
+  object Comparable {
+    implicit def byScalaOrd[A](implicit A: Ordering[A]): Comparable[A] = new Comparable[A] {
+      def lte(x: A, y: A): Boolean =
+        A.compare(x, y) <= 0
+    }
+  }
+
   implicit class EqualityOps[A](val x: A) extends AnyVal {
     def ===(y: A)(implicit A: Equality[A]): Boolean =
       A.eql(x, y)
 
     def ≠(y: A)(implicit A: Equality[A]): Boolean =
       A.neq(x, y)
+  }
+
+  implicit class ComparableOps[A](val x: A) extends AnyVal {
+    def ≤(y: A)(implicit A: Comparable[A]): Boolean =
+      A.lte(x, y)
   }
 }
--- src/test/scala/meetup/StdOpsSpec.scala
+++ src/test/scala/meetup/StdOpsSpec.scala
@@ -13,4 +13,13 @@ class StdOpsSpec extends FlatSpec with Matchers with NonImplicitAssertions {
     ("foo" === "bar") should be(false)
     """("foo" === 42)""" shouldNot typeCheck
   }
+
+  behavior of "Comparable"
+
+  it should "provide type-safe comparing" in {
+
+    ("foo" ≤ "foo") should be(true)
+    ("foo" ≤ "bar") should be(false)
+    """("foo" ≤ 42)""" shouldNot typeCheck
+  }
 }
