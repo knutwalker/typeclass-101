856d596693d156f7598971fc6d4bf423a930a9ac
Paul Horn <knutwalker@gmail.com>
2015-08-26 19:59:05 +0200

    Remake the std stuff: what, test first, eh?

---
+++ src/test/scala/meetup/StdOpsSpec.scala
@@ -0,0 +1,16 @@
+package meetup
+package std
+
+import org.scalatest._
+
+class StdOpsSpec extends FlatSpec with Matchers with NonImplicitAssertions {
+
+  behavior of "Equality"
+
+  it should "provide type-safe equality" in {
+
+    ("foo" === "foo") should be(true)
+    ("foo" === "bar") should be(false)
+    """("foo" === 42)""" shouldNot typeCheck
+  }
+}
